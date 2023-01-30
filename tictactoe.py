f06072b16d640c2a1f6895f1295e03800f6b246c
ssj.shahri <122171367+sajedejalali@users.noreply.github.com>
2023-01-30 10:15:15 +0330

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,106 @@
+import pyfiglet
+
+def show():
+    for row in game_board:
+        for cell in row :
+            print(cell," ", end=" ")
+        print()
+
+title = pyfiglet.figlet_format("tic tac toe", font="slant")
+print(title)
+
+
+def check_game():
+    
+    if game_board[0][0] == "X" and game_board[0][1] == "X" and game_board[0][2] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[0][0] == "O" and game_board[0][1] == "O" and game_board[0][2] == "O" :
+        print("player2 : you win!!!")
+    
+    if game_board[1][0] == "X" and game_board[1][1] == "X" and game_board[1][2] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[1][0] == "O" and game_board[1][1] == "O" and game_board[1][2] == "O" :
+        print("player2 : you win!!!")
+    
+    if game_board[2][0] == "X" and game_board[2][1] == "X" and game_board[2][2] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[2][0] == "O" and game_board[2][1] == "O" and game_board[2][2] == "O" :
+        print("player2 : you win!!!")
+    
+    if game_board[0][0] == "X" and game_board[1][0] == "X" and game_board[2][0] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[0][0] == "O" and game_board[1][0] == "O" and game_board[2][0] == "O" :
+        print("player2 : you win!!!")
+    
+    if game_board[0][1] == "X" and game_board[1][1] == "X" and game_board[2][1] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[0][1] == "O" and game_board[1][1] == "O" and game_board[2][1] == "O" :
+        print("player2 : you win!!!")
+    
+    if game_board[0][2] == "X" and game_board[1][2] == "X" and game_board[2][2] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[0][2] == "O" and game_board[1][2] == "O" and game_board[2][2] == "O" :
+        print("player2 : you win!!!")
+    
+    if game_board[0][0] == "X" and game_board[1][1] == "X" and game_board[2][2] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[0][0] == "O" and game_board[1][1] == "O" and game_board[2][2] == "O" :
+        print("player2 : you win!!!")
+    
+    if game_board[0][2] == "X" and game_board[1][1] == "X" and game_board[2][0] == "X" :
+        print("player1 : you win!!!")
+    
+    if game_board[0][2] == "O" and game_board[1][2] == "O" and game_board[2][0] == "O" :
+        print("player2 : you win!!!")   
+    
+    if game_board[0][0] != "-" and game_board[0][1] != "-" and game_board[0][2] != "-" and game_board[1][0] != "-" and game_board[1][1] != "-" and game_board[1][2] != "-" and game_board[2][0] != "-" and game_board[2][1] != "-" and game_board[2][2] != "-" :
+        print("drow!!!")    
+
+game_board = [["-", "-", "-"],
+                ["-", "-", "-"],
+                ["-", "-", "-"]]
+show()
+
+for i in range(10):
+    while True:
+        #player1
+        print("player1")
+        while True:
+            row = int(input("enter row : "))
+            col = int(input("enter col : "))
+            if 0 <= row <= 2 and 0 <= col <= 2:
+                if game_board[row][col] == "-" :
+                    game_board[row][col] = "X"
+                    show()
+                    check_game()
+                    break
+                else :
+                    print("enter row/col again : ")
+            else:
+                print("enter 0 <= row/col <= 2")
+        
+        #player2
+        print("player2")
+        while True:
+            row = int(input("enter row : "))
+            col = int(input("enter col : "))
+            if 0 <= row <= 2 and 0 <= col <= 2:
+                if game_board[row][col] == "-" :
+                    game_board[row][col] = "O"
+                    show()
+                    check_game()
+                    break
+                else :
+                    print("enter row/col again : ")
+            else :
+                print("enter 0 <= row/col <= 2")
+
+
+
